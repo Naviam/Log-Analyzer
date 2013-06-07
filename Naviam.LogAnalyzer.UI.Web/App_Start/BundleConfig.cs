d1d838bbcbbde0c176013274468b02c0aeeabad5
vshaporov <vshaporov@servicechannel.com>
2013-06-07 15:35:19 +0300

    filter popover

--- Naviam.LogAnalyzer.UI.Web/App_Start/BundleConfig.cs
+++ Naviam.LogAnalyzer.UI.Web/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace Naviam.DataAnalyzer.UI.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css", "~/Content/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
