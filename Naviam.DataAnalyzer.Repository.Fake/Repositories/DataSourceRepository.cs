868881db48bfab2f664f109f6740167fadd9cc80
PavelMironchik <pavel.mironchik@gmail.com>
2013-06-11 04:41:56 +0300

    update

---
+++ Naviam.DataAnalyzer.Repository.Fake/Repositories/DataSourceRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace Naviam.DataAnalyzer.Repository.Fake.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Naviam.DataAnalyzer.Model.DataSource;
+
+    public class DataSourceRepository : IDataSourceRepository
+    {
+        public DataSource GetDataSource(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DataSource> GetDataSources(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid AddDataSource(DataSource dataSource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSource UpdateDataSource(DataSource dataSource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDataSource(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Naviam.LogAnalyzer.Model/DataSource/IDataSourceRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Naviam.DataAnalyzer.Model.DataSource
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface IDataSourceRepository
+    {
+        DataSource GetDataSource(Guid id);
+
+        List<DataSource> GetDataSources(int userId);
+
+        Guid AddDataSource(DataSource dataSource);
+
+        DataSource UpdateDataSource(DataSource dataSource);
+
+        void DeleteDataSource(Guid id);
+    }
+}
