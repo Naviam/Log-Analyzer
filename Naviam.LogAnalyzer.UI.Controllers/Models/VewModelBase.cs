b1b1e028e943fc548d031d73c33f484470cf8b27
vshaporov <vshaporov@servicechannel.com>
2013-06-28 14:26:11 +0300

    ui+models

---
+++ Naviam.LogAnalyzer.UI.Controllers/Models/VewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Naviam.DataAnalyzer.UI.Controllers.Models
+{
+    public abstract class VewModelBase
+    {
+        public string ApiUrl
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["WebApiRootUrl"];
+            }
+        }
+    }
+}
